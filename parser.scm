c2a2a6373838dd8ccbc7ea875f2e9eeafefa0e68
jitwit <jrn@bluefarm.ca>
2019-10-21 16:41:18 -0400

    move examples to separate file

--- parser.scm
+++ parser.scm
@@ -262,65 +262,6 @@
 (define (count-matches lang S)
   (length (find-matches lang S)))
 
-(define lang-cadr
-  (re:. #\c
-        (re:1+ (re:+ #\a #\d))
-        #\r))
-
-(define lang-ab*
-  (re:. #\a (re:* #\b)))
-
-(define lang-ab*+c*ad
-  (re:+ lang-ab*
-        (re:. (re:* #\c)
-              #\a
-              #\d)))
-
-(define lower-ascii
-  (string->charset "abcdefghijklmnopqrstuvwxyz"))
-
-(define lang-ascii-letter
-  (string->charset "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"))
-
-(define lang-word
-  (re:1+ lower-ascii))
-
-(define lang-digit
-  (string->charset "0123456789"))
-
-(define lang-number
-  (re:1+ lang-digit))
-
-(define lang-whitespace
-  (list->charset '(#\tab #\space #\newline)))
-
-(define lang-1+1
-  (re:. #\1 (re:* (re:. #\+ #\1))))
-
-(define lang-n+n
-  (re:. lang-number
-        (re:* (re:. #\+ lang-number))))
-
-(define lang-float
-  (re:. (re:? (re:+ #\+ #\-))
-        (re:+ (re:1+ (re:& lang-digit (re:- #\0)))
-              #\0)
-        (re:? (re:. #\. (re:1+ lang-digit)))))
-
-(define lang-arith
-  (let ((op (re:. (re:* lang-whitespace)
-                  (string->charset "+*-/^%")
-                  (re:* lang-whitespace))))
-    (re:. lang-float
-          (re:* (re:. op lang-float)))))
-
-(define lang-loop
-  (re:* (re:+ (re:string "for")
-              (re:string "while")
-              (re:string "if")
-              (re:string "do")
-              (re:. lang-word #\.))))
-
 (define (show-matches lang S)
   (map (lambda (m)
          (substring S (car m) (cdr m)))
